f6319c308290d966bf35cafaf05a74e860c3d703
Bailey Meyers <baileymeyers1@gmail.com>
2026-02-18 14:07:58 -0500

    Configure resvg external package for Next 14

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ["@resvg/resvg-js"],
   experimental: {
     serverActions: {
       allowedOrigins: []
-    }
+    },
+    serverComponentsExternalPackages: ["@resvg/resvg-js"]
   }
 };
 
